586add18dd950672a1dba11bd692d59615606643
RithEvil <sovannarith22317@gmail.com>
2024-03-09 16:52:45 +0700

    first commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,109 @@
+import React, { useContext } from "react";
+import "./Main.css";
+import { assets } from "../../assets/assets";
+import { Context } from "../../context/Context";
+
+const Main = () => {
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+  } = useContext(Context);
+
+  return (
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="user_icon" />
+      </div>
+      <div className="main-container">
+        {!showResult ? (
+          <>
+            <div className="greet">
+              <p>
+                <span>Hello, Rith</span>
+              </p>
+              <p>How can I help you today?</p>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <p>Suggest beautiful places to see on an upcoming road trip</p>
+                <img src={assets.compass_icon} alt="compass_icon" />
+              </div>
+              <div className="card">
+                <p>Provide questions to help me prepare for an interview</p>
+                <img src={assets.bulb_icon} alt="bulb_icon" />
+              </div>
+              <div className="card">
+                <p>Explain what the keto diet is in simple terms</p>
+                <img src={assets.message_icon} alt="message_icon" />
+              </div>
+              <div className="card">
+                <p>Explain this code. Go step by step in detail</p>
+                <img src={assets.code_icon} alt="code_icon" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="result-title">
+              <img src={assets.user_icon} alt="user_icon" />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <img src={assets.gemini_icon} alt="gemini_icon" />
+              {loading ? (
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              )}
+            </div>
+          </div>
+        )}
+
+        <div className="main-bottom">
+          <div className="search-box">
+            <input
+              onChange={(e) => {
+                setInput(e.target.value);
+              }}
+              value={input}
+              type="text"
+              placeholder="Enter a prompt here"
+            />
+            <div>
+              <img src={assets.gallery_icon} alt="gallery_icon" />
+              <img src={assets.mic_icon} alt="mic_icon" />
+              {input ? (
+                <img
+                  onClick={() => {
+                    onSent();
+                  }}
+                  src={assets.send_icon}
+                  alt="send_icon"
+                />
+              ) : null}
+            </div>
+          </div>
+          <p className="bottom-info">
+            Gemini may display inaccurate info, including about people, so
+            double-check its responses.
+            <a href="https://support.google.com/gemini/answer/13594961?visit_id=638455656663808766-1565987819&p=privacy_notice&rd=1#privacy_notice">
+              Your privacy and Gemini Apps
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/config/gemini.js
@@ -0,0 +1,56 @@
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+const MODEL_NAME = "gemini-1.0-pro";
+// TODO: Put your gemini api key here in const API_KEY = "-----"
+const API_KEY = "AIzaSyDAvD4OCbHo0XdNLaUHeULyFE68Tt0KF4o";
+
+// TODO: Add 1 parameter is prompt
+async function runChat(prompt) {
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+
+  const generationConfig = {
+    temperature: 0.9,
+    topK: 1,
+    topP: 1,
+    maxOutputTokens: 2048,
+  };
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+  ];
+
+  const chat = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [],
+  });
+
+  // TODO: Add prompt in sendMessage("----")
+  const result = await chat.sendMessage(prompt);
+  const response = result.response;
+  console.log(response.text());
+  return response.text();
+}
+
+// TODO: change it from function to export default
+export default runChat;
